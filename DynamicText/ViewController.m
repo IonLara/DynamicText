dbf3ec5bdfbb20e25eb73fd110ac44431dbf61e0
SebasMac <ionrodriguezlara@gmail.com>
2023-09-14 22:12:45 -0700

    Finished Project

--- DynamicText/ViewController.m
+++ DynamicText/ViewController.m
@@ -8,10 +8,31 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *textField;
 
 @end
 
 @implementation ViewController
+- (IBAction)checkForRed:(UITextField *)sender {
+    NSString *lowercased = [sender.text lowercaseString];
+    NSArray *strings = [lowercased componentsSeparatedByString:@"red"];
+    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:sender.text];
+    NSMutableArray *ranges = [NSMutableArray new];
+    NSRange searchRange = NSMakeRange(0, [lowercased length]);
+    while (searchRange.location != NSNotFound) {
+        searchRange = [lowercased rangeOfString:@"red" options:0 range:searchRange];
+        if (searchRange.location != NSNotFound) {
+            [ranges addObject:[NSValue valueWithRange:searchRange]];
+            searchRange = NSMakeRange(NSMaxRange(searchRange), [lowercased length] - NSMaxRange(searchRange));
+        }
+    }
+    for(NSValue *rangeValue in ranges)
+    {
+        NSRange range = [rangeValue rangeValue];
+        [attributedString addAttribute:NSForegroundColorAttributeName value:[UIColor redColor] range:range];
+    }
+    sender.attributedText = attributedString;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
